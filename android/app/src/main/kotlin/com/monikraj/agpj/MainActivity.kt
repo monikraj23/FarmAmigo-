9604f54141e9b4e47b2a8517c8534dd0cdf26053
Inesh03 <inesh3624@gmail.com>
2025-04-20 06:09:17 +0530

    change in file structure

--- android/app/src/main/kotlin/com/monikraj/agpj/MainActivity.kt
+++ android/app/src/main/kotlin/com/monikraj/agpj/MainActivity.kt
@@ -1,5 +1,5 @@
-package com.monikraj.impciker
+    package com.monikraj.agpj
 
-import io.flutter.embedding.android.FlutterActivity
+    import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+    class MainActivity: FlutterActivity()
